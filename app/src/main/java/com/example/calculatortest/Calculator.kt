4e06aba9cc438032a7097d703f4737dba7090576
Nico Flassy <89779963+ncflsy@users.noreply.github.com>
2023-11-04 15:17:08 +0700

    add comment in main file

--- app/src/main/java/com/example/calculatortest/Calculator.kt
+++ app/src/main/java/com/example/calculatortest/Calculator.kt
@@ -1,6 +1,9 @@
 package com.example.calculatortest
 
 class Calculator {
+    // ini adalah function untuk melakukan penjumlahan 2 data. 2 data tersebut diambil dari 2 parameter, yaitu parameter firt dan second
+    // yang sama-sama menggunakan tipe data interger. kemudian function ini akan mengembalikan nilai dari kedua hasil penjumlahan nilai
+    // parameter tersebut
     fun add(first: Int, second: Int):Int{
         return first + second;
     }
